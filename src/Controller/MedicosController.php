f2d9764674c554471a85c5741016bf7b0122b8f4
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2019-09-18 14:58:13 -0300

    Finalizando CRUD

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\Medico;
+use App\Helper\MedicoFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -19,10 +20,18 @@ class MedicosController extends AbstractController
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var MedicoFactory
+     */
+    private $medicoFactory;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        MedicoFactory $medicoFactory
+    )
     {
         $this->entityManager = $entityManager;
+        $this->medicoFactory = $medicoFactory;
     }
 
     /**
@@ -32,12 +41,7 @@ class MedicosController extends AbstractController
     {
         $corpoRequisicao = $request->getContent();
 
-        $dadoEmJSON = json_decode($corpoRequisicao);
-
-        $medico = new Medico();
-
-        $medico->crm = $dadoEmJSON->crm;
-        $medico->nome = $dadoEmJSON->nome;
+        $medico = $this->medicoFactory->criarMedico($corpoRequisicao);
 
         $this->entityManager->persist($medico);
         //realizar varias operacoes com o banco
@@ -63,14 +67,62 @@ class MedicosController extends AbstractController
     /**
      * @Route("medicos/{id}", methods={"GET"})
      */
-    public function buscarUm(Request $request): Response
+    public function buscarUm(int $id): Response
     {
-        $id = $request->get('id');
-        $repositorioMedicos = $this->getDoctrine()->getRepository(Medico::class);
-        $medico = $repositorioMedicos->find($id);
+        $medico = $this->buscaMedico($id);
 
         $codigoResposta = is_null($medico) ? Response::HTTP_NO_CONTENT : 200;
 
         return new JsonResponse($medico, $codigoResposta);
+    }
+
+    /**
+     * @Route("medicos/{id}", methods={"PUT"})
+     */
+    public function alteraMedico(int $id, Request $request): Response
+    {
+        $corpoReq = $request->getContent();
+        $dadoEmJson = json_decode($corpoReq);
+
+        $medico = $this->buscaMedico($id);
+
+        if (is_null($medico)) {
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        $medico->crm = $dadoEmJson->crm;
+        $medico->nome = $dadoEmJson->nome;
+
+        $this->entityManager->flush();
+
+        return new JsonResponse($medico);
+    }
+
+    /**
+     * @Route("medicos/{id}", methods={"DELETE"})
+     */
+    public function deleta(int $id): Response
+    {
+        $medico = $this->buscaMedico($id);
+
+        if(is_null($medico)){
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        $this->entityManager->remove($medico);
+        $this->entityManager->flush();
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @param int $id
+     * @return object|null
+     */
+    public function buscaMedico(int $id)
+    {
+        $repositorioMedicos = $this->getDoctrine()->getRepository(Medico::class);
+        $medico = $repositorioMedicos->find($id);
+        return $medico;
     }
 }
---
+++ src/Helper/MedicoFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Helper;
+
+
+use App\Entity\Medico;
+
+class MedicoFactory
+{
+    public function criarMedico(string $json): Medico
+    {
+        $dadoEmJson = json_decode($json);
+
+        $medico = new Medico();
+
+        $medico->crm = $dadoEmJson->crm;
+        $medico->nome = $dadoEmJson->nome;
+
+        return $medico;
+    }
+}
